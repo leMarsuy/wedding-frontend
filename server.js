12c040ad39086ffdfed738ff21d9491c9da62adf
Marci <marci@lgusuite.com>
2024-05-24 19:02:20 +0800

    added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/wedding-frontend/"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/wedding-frontend/index.html"));
+});
+
+const port = process.env.PORT || "4998";
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
